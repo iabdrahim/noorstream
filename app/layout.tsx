16fb9f637761d49569323d4d56133f1e57bcce86
abderrahim101 <abderrahimaneddam1@gmail.com>
2023-10-01 10:18:09 +0100

    git init

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,31 +4,31 @@ import { Tajawal, Noto_Sans_Arabic } from "next/font/google";
 import Providers from "../components/provider";
 
 export const tajawal = Tajawal({
-    weight: ["200", "300", "400", "500", "700", "800", "900"],
-    subsets: ["arabic"],
+  weight: ["200", "300", "400", "500", "700", "800", "900"],
+  subsets: ["arabic"],
 });
 
 const noto = Noto_Sans_Arabic({
-    weight: ["300", "400", "700", "800"],
-    subsets: ["arabic"],
+  weight: ["300", "400", "700", "800"],
+  subsets: ["arabic"],
 });
 
-export const metadata: Metadata = {
-    title: "Noonstream",
+const metadata: Metadata = {
+  title: "Noonstream",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="ar" className="dark">
-            <body
-                className={`${tajawal.className} bg-day dark:bg-night text-gray-900 dark:text-gray-100`}
-            >
-                <Providers>{children}</Providers>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="ar" className="dark">
+      <body
+        className={`${tajawal.className} bg-day dark:bg-night text-gray-900 dark:text-gray-100`}
+      >
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
 }
