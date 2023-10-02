74e573418d1bbbcf01329b689af8e457cb5e6ad0
abderrahim101 <abderrahimaneddam1@gmail.com>
2023-10-02 09:13:03 +0100

    commit

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -3,30 +3,30 @@
 import Container from "@/components/Container";
 import Cards from "@/components/ui/cards";
 import { providerContext } from "@/context/providers";
-import { IMovie } from "@/utils/providers/list";
+import { ICard } from "@/utils/providers/list";
 import React, { useContext, useState, useEffect } from "react";
 
 function Search({
-    searchParams,
+  searchParams,
 }: {
-    searchParams?: { [key: string]: string | undefined };
+  searchParams?: { [key: string]: string | undefined };
 }) {
-    let { provider } = useContext(providerContext);
-    let [data, setData] = useState<IMovie[] | null>(null);
-    useEffect(() => {
-        let get = async () => {
-            let Class = new provider.class();
-            let res = await Class.search(searchParams?.q || "");
-            console.log(res);
-            setData(res);
-        };
-        get();
-    }, []);
-    return (
-        <Container>
-            <Cards data={data} />
-        </Container>
-    );
+  let { provider } = useContext(providerContext);
+  let [data, setData] = useState<ICard[] | null>(null);
+  useEffect(() => {
+    let get = async () => {
+      let Class = new provider.class();
+      let res = await Class.search(searchParams?.q || "");
+      console.log(res);
+      setData(res);
+    };
+    get();
+  }, []);
+  return (
+    <Container>
+      <Cards data={data} />
+    </Container>
+  );
 }
 
 export default Search;
