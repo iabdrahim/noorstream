fe324aec364f7145f976d4ff002f9f466bf67c0c
abderrahim101 <abderrahimaneddam1@gmail.com>
2023-10-02 09:26:17 +0100

    commit

--- components/hero.tsx
+++ components/hero.tsx
@@ -2,19 +2,19 @@
 import { useState, useEffect, useContext } from "react";
 import Cards from "./ui/cards";
 import { providerContext } from "@/context/providers";
-import { IMovie } from "@/utils/providers/list";
+import { ICard } from "@/utils/providers/list";
 
 export default function Hero() {
-    let { provider } = useContext(providerContext);
-    let [data, setData] = useState<IMovie[] | null>(null);
-    useEffect(() => {
-        let get = async () => {
-            let Class = new provider.class();
-            let res = await Class.getMainPage();
-            console.log("data", res);
-            setData(res);
-        };
-        get();
-    }, []);
-    return <Cards data={data} />;
+  let { provider } = useContext(providerContext);
+  let [data, setData] = useState<ICard[] | null>(null);
+  useEffect(() => {
+    let get = async () => {
+      let Class = new provider.class();
+      let res = await Class.getMainPage();
+      console.log("data", res);
+      setData(res);
+    };
+    get();
+  }, []);
+  return <Cards data={data} />;
 }
